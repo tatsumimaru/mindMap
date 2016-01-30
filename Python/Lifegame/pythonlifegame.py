12b59e72a6dddadb735a5c4b94e631b11f7524c5
tatsumimaru <tatsumimaru@users.noreply.github.com>
2016-01-30 23:31:29 +0900

    Update pythonlifegame.py

--- Python/Lifegame/pythonlifegame.py
+++ Python/Lifegame/pythonlifegame.py
@@ -20,8 +20,22 @@ def init():
         field.append(row)
     draw()
 
-#def rand_set():
-#def reset():
+def rand_set():
+    if is_run:
+        return
+    for i in range(height):
+        for j in range(width):
+            field[i][j] = random.choice(status)
+    draw()
+    
+def reset():
+    if is_run:
+        return
+    for i in range(height):
+        for j in range(width):
+            field[i][j] = dead
+    draw()
+    
 def draw():
     canvas.delete("field")
     for i in range(height):
@@ -33,8 +47,32 @@ def draw():
             canvas.create_rectangle (x0, y0, x1, y1, fill = color [field[i][j]], tags = "field")
             
 
-#def next():
-#def count(x, y):
+def next():
+    global field
+    new_field = []
+    for i in range(height):
+        row = []
+        for j in range(width):
+            num = count(i, j)
+            if num == 3:
+                row.append(alive)
+            elif num == 2:
+                row.append(field[i][j])
+            else:
+                row.append(dead)
+        new_field.append(row)
+    field = new_field
+    draw()
+
+def count(x, y):
+    num = 0
+    
+    if x! = 0:
+        num = num + field[y][x - 1]
+        if y! = 0:
+            num = num + field[y - 1][x - 1]
+        if y! = height -1:
+            num = num + field[y + 1][x + !] 
 #def run():
 #def start_stop():
 #def alive_dead(event):
