c03dbbf199e6e9983c4304ad1520eb5a8a06cc45
tatsumimaru <tatsumimaru@users.noreply.github.com>
2016-01-31 23:06:33 +0900

    Bug fix on pythonlifegame.py

--- Python/Lifegame/pythonlifegame.py
+++ Python/Lifegame/pythonlifegame.py
@@ -12,7 +12,7 @@ height = 50
 width = 50
 field = []
 
-#random.seed(0)
+random.seed(0)
 
 def init():
     for i in range(height):
@@ -54,7 +54,7 @@ def next():
     for i in range(height):
         row = []
         for j in range(width):
-            num = count(i, j)
+            num = count(j, i)
             if num == 3:
                 row.append(alive)
             elif num == 2:
