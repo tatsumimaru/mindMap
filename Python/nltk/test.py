6c040ba9b55231206de9bbc07930a659e19d3e67
tatsumimaru <tatsumimaru@users.noreply.github.com>
2016-02-16 22:03:35 +0900

    Bug fix on /Python/nltk/test.py

--- Python/nltk/test.py
+++ Python/nltk/test.py
@@ -3,7 +3,7 @@
 #This tiny script is available in both Python2.x and Python3.x.
 
 import nltk.data
-tokenizer = nltk.data.load('tokenizers/punkt/japanese.pickle') 
-para = "日本語でもうまくいくかな？どうかな。どうかなどうかな。"
+tokenizer = nltk.data.load('tokenizers/punkt/english.pickle') 
+para = "Hello World. I'm playfully meeting nltk for the first time. "
 print(tokenizer.tokenize(para))
 
